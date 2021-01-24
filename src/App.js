04777d8f3b975ffd198bd881538e6b10e053de03
dong uk <gusrn1255@naver.com>
2021-01-24 11:19:28 +0900

    Add GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Router from "./components/Router";
-
+import GlobalStyles from "./components/GlobalStyles";
 const App = () => {
     return (
         <>
 
             <Router />
+            <GlobalStyles />
         </>
 
     );
