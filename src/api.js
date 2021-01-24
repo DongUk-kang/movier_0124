2118aafa8e27db16fc68f57ff3b1256aaca62546
dong uk <gusrn1255@naver.com>
2021-01-24 13:14:01 +0900

    api add

--- src/api.js
+++ src/api.js
@@ -1,34 +1,59 @@
 import axios from "axios";
 
 
-const makeRequest = (path, params) => {
+// const makeRequest = (path, params) => {
+//     axios.get(`https://api.themoviedb.org/3${path}`, {
+//         params: {
+//             ...params,
+//             api_key: "c9349dd29b0c396b729d9fc6016daf67"
+//         }
+//     })
+// }
+
+const makeRequest = (path, params) =>
     axios.get(`https://api.themoviedb.org/3${path}`, {
         params: {
             ...params,
             api_key: "c9349dd29b0c396b729d9fc6016daf67"
-        }
-    })
-}
+        },
+    });
 
+// const getAnything = async (path, params = {}) => {
+//     try {
+//         const {
+//             data: { results },
+//             data
+//         } = await makeRequest(path, params)
+//         return [results || data, null]
+//
+//
+//     } catch (e) {
+//         return [null, e]
+//     }
 
 const getAnything = async (path, params = {}) => {
     try {
         const {
-            data: { results }
-        } = await makeRequest(path, params)
-        return [results, null]
-
-
+            data: { results },
+            data,
+        } = await makeRequest(path, params);
+        return [results || data, null];
     } catch (e) {
-        return [null, e]
+        console.log(e);
+        return [null, e];
     }
+};
 
-}
+
 
 export const movieAPI = {
-    nowPlaying: () => getAnything('/movie/now_playing'),
-    topRated: () => getAnything('/movie/top_rated'),
-    upComing: () => getAnything('/movie/upcoming')
+    // nowPlaying: () => getAnything('/movie/now_playing'),
+    // topRated: () => getAnything('/movie/top_rated'),
+    // upComing: () => getAnything('/movie/upcoming')
+
+    nowPlaying: () => getAnything("/movie/now_playing"),
+    topRated: () => getAnything("/movie/top_rated"),
+    upComing: () => getAnything("/movie/upcoming"),
 
 }
 
--- src/srceens/HomeScreen.js
+++ src/srceens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {Card} from "react-bootstrap";
 import styled from "styled-components";
-import axios from "axios";
+
+import {movieAPI} from "../api"
 
 const Container = styled.div`
   padding: 40px 30px;
@@ -13,47 +13,82 @@ const Title = styled.h1`
   font-weight: 500;
 `
 
-const Grid = styled.div`
-  margin-top: 25px;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 300px);
-  grid-gap: 25px;
-`
+// const Grid = styled.div`
+//   margin-top: 25px;
+//   display: grid;
+//   grid-template-columns: repeat(auto-fill, 300px);
+//   grid-gap: 25px;
+// `
 
 const HomeScreen = () => {
     const [upcomings, setUpcomings] = useState([])
     const [topRates, setRates] = useState([])
+    const [nowPlaying, setNowPlaying] = useState([])
     const [loading, setLoading] = useState(true)
 
-    const getUpcoming = async () => {
-        return(
-            await axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=c9349dd29b0c396b729d9fc6016daf67&language=en-US&page=1')
-                // .then(aaa => console.log(aaa.data.results))
-                .then(aaa => {
-                    setUpcomings(aaa.data.results)
-                    setLoading(false)
-                })
+    // const [movies, setMovies] = useState({
+    //     nowPlaying: [],
+    //     topRated: [],
+    //     upComing: [],
+    //     loading: true
+    // })
 
-                .catch(err => console.log(err))
-        )
-    }
 
-    const getRate = async () => {
-        return(
-            await axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=c9349dd29b0c396b729d9fc6016daf67&language=en-US&page=1')
-                // .then(bbb => console.log(bbb.data.results))
-                .then(bbb => {
-                    setRates(bbb.data.results)
-                    setLoading(false)
-                })
-                .catch(err => console.log(err))
-        )
+    // const getUpcoming = async () => {
+    //     return(
+    //         await axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=c9349dd29b0c396b729d9fc6016daf67&language=en-US&page=1')
+    //             // .then(aaa => console.log(aaa.data.results))
+    //             .then(aaa => {
+    //                 setUpcomings(aaa.data.results)
+    //                 setLoading(false)
+    //             })
+    //
+    //             .catch(err => console.log(err))
+    //     )
+    // }
+    //
+    // const getRate = async () => {
+    //     return(
+    //         await axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=c9349dd29b0c396b729d9fc6016daf67&language=en-US&page=1')
+    //             // .then(bbb => console.log(bbb.data.results))
+    //             .then(bbb => {
+    //                 setRates(bbb.data.results)
+    //                 setLoading(false)
+    //             })
+    //             .catch(err => console.log(err))
+    //     )
+    // }
+
+    const getMovies = async () => {
+       const [nowPlaying, nowPlayingError] = await movieAPI.nowPlaying()
+
+        const [upComing, upComingError] = await movieAPI.upComing()
+
+        const [topRated, topRatedError] = await movieAPI.topRated()
+
+        setUpcomings(upComing)
+        setRates(topRated)
+        setNowPlaying(nowPlaying)
+        setLoading(false)
+
+
+
+        // setMovies({
+        //     nowPlaying: nowPlaying,
+        //     topRated: topRated,
+        //     upComing: upComing,
+        //     loading: false
+        // })
+
+        console.log(nowPlaying.length)
+
     }
 
 
     useEffect(() => {
-        getUpcoming()
-        getRate()
+        // getUpcoming()
+        // getRate()
+        getMovies()
     },[])
 
     return (
@@ -68,6 +103,7 @@ const HomeScreen = () => {
                 <>
                     <div>
                         <Title> 업커밍 </Title>
+
                         {upcomings.map(movie => (
                             <h1>{movie.title}</h1>
                         ))}
@@ -76,6 +112,13 @@ const HomeScreen = () => {
                     <div>
                         <Title> 트렌드 </Title>
                         {topRates.map(movie => (
+                            <h1>{movie.title}</h1>
+                        ))}
+
+                    </div>
+                    <div>
+                        <Title> 업커밍 </Title>
+                        {upcomings.map(movie => (
                             <h1>{movie.title}</h1>
                         ))}
 
